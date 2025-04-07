d71a1b3a845962b01731b117756f3be536c4dc74
Rejeq <jere3690@gmail.com>
2025-04-07 15:19:04 +0300

    chore: Add VideoRelay and VideoTarget docs

--- core/stream/src/main/kotlin/com/rejeq/cpcam/core/stream/relay/VideoRelay.kt
+++ core/stream/src/main/kotlin/com/rejeq/cpcam/core/stream/relay/VideoRelay.kt
@@ -2,6 +2,15 @@ package com.rejeq.cpcam.core.stream.relay
 
 import android.view.Surface
 
+/**
+ * Interface for relaying raw video frames to an underlying video encoder.
+ *
+ * This interface defines the contract for components that forward raw,
+ * unencoded video data to a video encoder. Implementations of this interface
+ * act as a bridge between a video source (e.g., camera, screen capture) and an
+ * encoder. The encoder uses the provided `Surface` to receive the raw frames
+ * and process them for encoding.
+ */
 interface VideoRelay {
     val surface: Surface
 
--- core/stream/src/main/kotlin/com/rejeq/cpcam/core/stream/target/VideoTarget.kt
+++ core/stream/src/main/kotlin/com/rejeq/cpcam/core/stream/target/VideoTarget.kt
@@ -3,6 +3,10 @@ package com.rejeq.cpcam.core.stream.target
 import android.util.Range
 import com.rejeq.cpcam.core.stream.relay.VideoRelay
 
+/**
+ * Responsible for fetching video data from a specific source
+ * and relaying it to a [VideoRelay].
+ */
 interface VideoTarget {
     fun setRelay(relay: VideoRelay)
 
