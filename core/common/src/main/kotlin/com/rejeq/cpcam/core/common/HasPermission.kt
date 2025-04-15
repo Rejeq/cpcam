5eb5dca28dfa03d2c74cce4be8ef981edd746de5
Rejeq <jere3690@gmail.com>
2025-04-15 18:42:45 +0300

    chore: Docs

--- core/common/src/main/kotlin/com/rejeq/cpcam/core/common/HasPermission.kt
+++ core/common/src/main/kotlin/com/rejeq/cpcam/core/common/HasPermission.kt
@@ -4,6 +4,15 @@ import android.content.Context
 import android.content.pm.PackageManager
 import androidx.core.app.ActivityCompat
 
+/**
+ * Convenient way to check if the application has a specific permission.
+ *
+ * @param permission The permission to check
+ *        (e.g., `android.Manifest.permission.CAMERA`).
+ * @return `true` if the permission is granted, `false` otherwise.
+ *
+ * @see ActivityCompat.checkSelfPermission
+ */
 fun Context.hasPermission(permission: String): Boolean =
     ActivityCompat.checkSelfPermission(this, permission) ==
         PackageManager.PERMISSION_GRANTED
