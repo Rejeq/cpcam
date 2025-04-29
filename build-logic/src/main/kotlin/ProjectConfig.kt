60ace873c5e971159624003533573878403a14fb
Rejeq <jere3690@gmail.com>
2025-04-29 11:45:27 +0300

    chore: Increase target sdk version

--- build-logic/src/main/kotlin/ProjectConfig.kt
+++ build-logic/src/main/kotlin/ProjectConfig.kt
@@ -1,6 +1,6 @@
 object ProjectConfig {
-    const val COMPILE_SDK = 35
-    const val TARGET_SDK = 35
+    const val COMPILE_SDK = 36
+    const val TARGET_SDK = 36
     const val MIN_SDK = 21
 
     const val JVM_TARGET = "17"
