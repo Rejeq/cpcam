165a44ab15962de17e185f5ec46c39259bb3dcb5
Rejeq <jere3690@gmail.com>
2025-06-23 17:20:05 +0300

    chore: Cleanup code

--- feature/service/src/main/kotlin/com/rejeq/cpcam/feature/service/ConnectionErrorComponent.kt
+++ feature/service/src/main/kotlin/com/rejeq/cpcam/feature/service/ConnectionErrorComponent.kt
@@ -4,17 +4,12 @@ import androidx.compose.runtime.Immutable
 import com.arkivanov.decompose.ComponentContext
 import com.rejeq.cpcam.core.endpoint.EndpointErrorKind
 
-interface DialogComponent {
-    val onFinished: () -> Unit
-}
-
 class ConnectionErrorComponent(
     componentContext: ComponentContext,
     val reason: EndpointErrorKind,
-    override val onFinished: () -> Unit,
+    val onFinished: () -> Unit,
     val openEndpointSettings: () -> Unit,
-) : DialogComponent,
-    ComponentContext by componentContext
+) : ComponentContext by componentContext
 
 @Immutable
 data class ConnectionError(val title: String, val desc: String)
